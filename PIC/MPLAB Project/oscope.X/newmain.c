576bc7b57248fec35fb01792ec4e07abbc6ed9da
just4brown <justinbrown@cedarville.edu>
2014-10-04 18:25:04 -0400

    bluetooth 1 - not working

--- PIC/MPLAB Project/oscope.X/newmain.c
+++ PIC/MPLAB Project/oscope.X/newmain.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <p32xxxx.h>
 #include <plib.h>
+#include <peripheral/uart.h>
+
 
 // DEVCFG1
 #pragma config FNOSC = FRCDIV           // Oscillator Selection Bits (Fast RC Osc w/Div-by-N (FRCDIV))
@@ -30,75 +32,38 @@
 #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
 #pragma config CP = OFF                 // Code Protect (Protection Disabled)
 
-#define SYSCLK (40000000)
+// Defines
+#define SYSCLK 40000000
+#define PB_CLOCK 20000000
 
-int analogRead(char analogPIN){
-    AD1CHS = analogPIN << 16;       // AD1CHS<16:19> controls which analog pin goes to the ADC
+void main() {
 
-    AD1CON1bits.SAMP = 1;           // Begin sampling
-    while( AD1CON1bits.SAMP );      // wait until acquisition is done
-    while( ! AD1CON1bits.DONE );    // wait until conversion done
+     // Peripheral Pin Select
+    U2RXRbits.U2RXR = 4;    //SET RX to RB8
+    RPB9Rbits.RPB9R = 2;    //SET RB9 to TX
+    //int PB_CLOCK = 20000000;
+    int desiredBAUD = 9600;
 
-    return ADC1BUF0;                // result stored in ADC1BUF0
-}
+    // Calculate actual assigned baud rate
+    int actual_baud = PB_CLOCK / (4 * desiredBAUD) - 1;
 
-void delay_us( unsigned t)          // See Timers tutorial for more info on this function
-{
-    T1CON = 0x8000;                 // enable Timer1, source PBCLK, 1:1 prescaler
+    OpenUART2(UART_EN|UART_BRGH_FOUR, UART_RX_ENABLE | UART_TX_ENABLE, actual_baud);
 
-    // delay 100us per loop until less than 100us remain
-    while( t >= 100){
-        t-=100;
-        TMR1 = 0;
-        while( TMR1 < SYSCLK/10000);
-    }
+    char filename[50] ;                         //Array of 50 chars
+    memset(filename,0,50*sizeof(char));         //Clears the Array
+    sprintf(filename, "Tutorial 4 ? BOSCOPE\nIncrementing Variable\n");    //Places the String into the Array
+    putsUART2(filename);                        //Sends the Array over UART1
 
-    // delay 10us per loop until less than 10us remain
-    while( t >= 10){
-        t-=10;
-        TMR1 = 0;
-        while( TMR1 < SYSCLK/100000);
-    }
-
-    // delay 1us per loop until finished
-    while( t > 0)
+    int variable = 0;
+    int index = 0;
+    while (1)
     {
-        t--;
-        TMR1 = 0;
-        while( TMR1 < SYSCLK/1000000);
+        sprintf(filename, "%d\n",variable); //%d will get the value of the next argument in the function and place it in the string
+        putsUART2(filename);                //Sends the Array over UART1
+        variable++;                         //Increments the variable by 1
+
+        for(index=0;index<4000000;index++) {
+            Nop();
+        }//a dummy function ot delay the loop
     }
-
-    // turn off Timer1 so function is self-contained
-    T1CONCLR = 0x8000;
-} // END delay_us()
-
-void adcConfigureManual(){
-    AD1CON1CLR = 0x8000;    // disable ADC before configuration
-
-    AD1CON1 = 0x00E0;       // internal counter ends sampling and starts conversion (auto-convert), manual sample
-    AD1CON2 = 0;            // AD1CON2<15:13> set voltage reference to pins AVSS/AVDD
-    AD1CON3 = 0x0f01;       // TAD = 4*TPB, acquisition time = 15*TAD
-} // END adcConfigureManual()
-
-int main( void)
-{
-	SYSTEMConfigPerformance(SYSCLK);
-
-        // Configure pins as analog inputs
-        ANSELBbits.ANSB3 = 1;   // set RB3 (AN5) to analog
-        TRISBbits.TRISB3 = 1;   // set RB3 as an input
-        TRISBbits.TRISB5 = 0;   // set RB5 as an output (note RB5 is a digital only pin)
-
-        adcConfigureManual();   // Configure ADC
-        AD1CON1SET = 0x8000;    // Enable ADC
-
-        int foo;
-	while ( 1)
-	{
-            foo = analogRead( 5); // note that we call pin AN5 (RB3) by it's analog number
-            delay_us( foo);       // delay according to the voltage at RB3 (AN5)
-            LATBINV = 0x0020;     // invert the state of RB5
-	}
-
-	return 0;
 }
